dbc07ae7b33a903393942037e748d53a218f6b8f
John Doe <zkochurin@mail.ru>
2020-05-06 20:20:16 +0700

    create component save

---
+++ src/components/save.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="save row justify-content-center">
+    <div class="col-12">
+      <h1 v-if="savedData" class=" text-center">Тут пока что пусто</h1>
+      <div v-else class="saved-data">
+        <h1 class=" text-center">Вы ввели:</h1>
+        <ul>
+          <li v-for="data in savedData" :key="data.id">{{data}}</li>
+        </ul>       
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'save',
+    data(){
+      return{
+
+      }
+    },
+    props: {
+      savedData:{
+        type: Array,
+        default(){
+          return []
+        } 
+      }
+    },
+    mounted(){
+
+    }
+}
+</script>
+
+<style>
+
+</style>
